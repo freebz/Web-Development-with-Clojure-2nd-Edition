7b1b419014c7825630af7501006ff73edef1337d
fx <fx@P54M.mshome.net>
2023-02-09 09:14:17 +0900

    5. Real-time Messaging With Websockets

---
+++ ch05/guestbook-websockets/src/clj/guestbook/routes/home.clj
@@ -0,0 +1,16 @@
+(ns guestbook.routes.home
+  (:require [guestbook.layout :as layout]
+            [guestbook.db.core :as db]
+            [compojure.core :refer [defroutes GET POST]] 
+            [ring.util.response :refer [response, status]]))
+
+(defn home-page []
+  (layout/render "home.html"))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/messages" [] (response (db/get-messages)))
+  (GET "/about" [] (about-page)))
